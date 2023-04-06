1534a4982a9562b16b517efdfb89c0f97e694099
Stephy <atienostephanie040@gmail.com>
2023-04-06 09:59:33 +0000

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,12 +8,8 @@ def minOperations(n):
     """
     Calculates minimum operations
     """
-    if n == 1:
+    if n <= 1:
         return 0
-    dp = [float('inf')] * (n+1)
-    dp[1] = 0
-    for i in range(2, n+1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i//j)
-    return dp[n] if dp[n] != float('inf') else 0
+    for num in range(2, n+1):
+        if n % num == 0:
+            return minOperations(int(n/num)) + num
